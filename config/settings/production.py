be3ef98163b73f506531f728d140fd5d37e935a5
Mevin Babu <mevinbabuc@gmail.com>
2017-09-09 08:18:02 +0530

    Added the admin URL

--- config/settings/production.py
+++ config/settings/production.py
@@ -199,7 +199,7 @@ LOGGING = {
 }
 
 # Custom Admin URL, use {% url 'admin:index' %}
-ADMIN_URL = env('DJANGO_ADMIN_URL')
+ADMIN_URL = r'^admin/'
 
 # Your production stuff: Below this line define 3rd party library settings
 # ------------------------------------------------------------------------------
